88238346cf0b8f258901a3bcc8ad364e8fb69909
jenboes <jlboes@ameritech.net>
2016-12-31 16:00:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+# dog
+class Dog
+  def name=(fullname)
+    @name = fullname
+  end
+  def name
+    @name
+  end
+  def breed=(fullname)
+    @breed = fullname
+  end
+  def breed
+    @breed
+  end
+end
