6fbbfa1c5ba625c7394513c524b4e1f2b27b21a7
Brian Navaluna <just.brian.en@mail.com>
2017-03-18 18:29:04 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+
+  def name=(some_name)
+    @name = some_name
+  end
+  def job=(some_job)
+    @job = some_job
+  end
+
+end
