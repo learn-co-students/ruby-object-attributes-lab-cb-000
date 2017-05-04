840cfac3e15f862676750474c4b5592f17e2cb40
Daniel Vimont <daniel.vimont@gmail.com>
2017-05-04 03:38:11 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(inputted_name)
+    @name = inputted_name
+  end
+
+  def name
+    return @name
+  end
+  
+  def job=(inputted_value)
+    @job = inputted_value
+  end
+
+  def job
+    return @job
+  end
+ 
+end
