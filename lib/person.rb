71924e86b7ae497d40a1618ee945acf49b3a142b
coryjanowski <coryjanowski@icloud.com>
2017-09-04 16:21:33 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+	def name=(name)
+		@name = name
+	end
+  def name
+  	@name
+  end
+
+ def job=(job)
+ 	@job = job
+ end
+ def job
+ 	@job
+ end
+
+end
+
+cory = Person.new
+cory.name = "Cory"
+ position = Person.new
+ position.name = "Programmer"
+
+
