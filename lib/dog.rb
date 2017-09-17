9bd546a44196a008ba62d0051d7263ad25c8b725
JR <jrfrazier21@gmail.com>
2017-09-17 10:45:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def instance(name)
+    @name = name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name
+    @name
+  end
+
+
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+end
