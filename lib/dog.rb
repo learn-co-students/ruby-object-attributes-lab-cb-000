79119df8d7de1949cea73ced88bb0f337945b89e
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-15 19:59:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(name_new)
+    @name = name_new
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_new)
+    @breed = breed_new
+  end
+
+  def breed
+    @breed
+  end
+end
